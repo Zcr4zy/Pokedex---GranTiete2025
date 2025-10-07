ab978ce11e772e717d7cf57d8fc57a1025479da1
GilmarFilho✔️ <gilmarfranciscodosantosfilho@outlook.com>
2025-10-06 21:06:23 -0300

    Add Details View

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Pokedex.Data;
 using Pokedex.Models;
+using Pokedex.ViewModels;
 
 namespace Pokedex.Controllers;
 
@@ -21,6 +22,18 @@ public class HomeController : Controller
     {
         var pokemons = _db.Pokemons.Include(i => i.Tipos).ThenInclude(i => i.Tipo).ToList();
         return View(pokemons);
+    }
+
+    public IActionResult Details(int id)
+    {
+        var pokemon = _db.Pokemons.Where(f => f.Numero == id).Include(i => i.Genero).Include(i => i.Regiao).Include(i => i.Tipos).ThenInclude(i => i.Tipo).SingleOrDefault();
+        DetailsVM details = new()
+        {
+            Atual = pokemon,
+            Anterior = _db.Pokemons.OrderByDescending(o => o.Numero).FirstOrDefault(f => f.Numero < id),
+            Proximo = _db.Pokemons.OrderBy(o => o.Numero).FirstOrDefault(f => f.Numero > id)
+        };
+        return View(details);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
